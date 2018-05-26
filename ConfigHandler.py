f7ab794f5ac28a9cb42ca555dd500f22e2715472
AlexBMJ <AlexBMJ@users.noreply.github.com>
2018-05-26 23:57:31 +0200

    Config Handler Fixes

--- ConfigHandler.py
+++ ConfigHandler.py
@@ -2,37 +2,34 @@ import os, sys
 from tkinter import messagebox
 
 class ReadConfig():
-	def __init__(self, folderpath):
-		self.folderpath = folderpath
-		self.main()
-
-	def main(self):
-			if (os.path.exists(self.folderpath + '\\bin') == False):
-				messagebox.showerror("bin folder missing", 'The "bin" folder is missing.\nPlease install the missing assets!')
-				os.remove(self.folderpath + '\\bin\\config.txt')
-				sys.exit()
-			if (os.path.exists(self.folderpath + '\\bin\\config.txt') == False):
-				self.config_dict = {'DefaultName':"Anonymous",'PeerPort':"4422",'Blacklist':"[""]",'LastConnectedIP':""}
-				write_config = str(self.config_dict).replace('{','{\n').replace('}','\n}').replace(',',',\n').replace(' ', '')
-				configfile = open(self.folderpath + '\\bin\\config.txt', 'w')
-				configfile.write(write_config)
-				configfile.close()
-			configfile = open(self.folderpath + '\\bin\\config.txt', 'r')
-			cfg_content = configfile.read().replace('\n','')
+	def main(self, folderpath):
+		if (os.path.exists(folderpath + '\\bin') == False):
+			messagebox.showerror("bin folder missing", 'The "bin" folder is missing.\nPlease install the missing assets!')
+			os.remove(folderpath + '\\bin\\config.txt')
+			sys.exit()
+		if (os.path.exists(folderpath + '\\bin\\config.txt') == False):
+			config_dict = {'DefaultName':"Anonymous",'PeerPort':"4422",'Blacklist':"[""]",'LastConnectedIP':""}
+			write_config = str(config_dict).replace('{','{\n').replace('}','\n}').replace(',',',\n').replace(' ', '')
+			configfile = open(folderpath + '\\bin\\config.txt', 'w')
+			configfile.write(write_config)
 			configfile.close()
-			if cfg_content.startswith('{') and cfg_content.endswith('}') and cfg_content.count('{') == 1 and cfg_content.count('}') == 1:
-				try:
-					return eval(cfg_content)
-				except:
-					self.error()
-			else:
-				self.error()
+			return config_dict
+		configfile = open(folderpath + '\\bin\\config.txt', 'r')
+		cfg_content = configfile.read().replace('\n','')
+		configfile.close()
+		if cfg_content.startswith('{') and cfg_content.endswith('}') and cfg_content.count('{') == 1 and cfg_content.count('}') == 1:
+			try:
+				return eval(cfg_content)
+			except:
+				error()
+		else:
+			error()
 
-	def error(self):
-		msgbox_result = messagebox.askyesno("Config File Error", '"config.txt" is either corrupt or has missing data.\nWould you like to create a new one?')
-		if (msgbox_result == True):
-			os.remove(self.folderpath + '\\bin\\config.txt')
-		sys.exit()
+		def error():
+			msgbox_result = messagebox.askyesno("Config File Error", '"config.txt" is either corrupt or has missing data.\nWould you like to create a new one?')
+			if (msgbox_result == True):
+				os.remove(folderpath + '\\bin\\config.txt')
+			sys.exit()
 
 if __name__ == '__main__':
 	print('module file')
